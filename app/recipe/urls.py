3422b561f0be0020fc17fe94f16c924562b631dd
Yahya Farah <yahya.f@opensooq.com>
2021-04-09 07:49:07 +0300

    add recipe features

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,7 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngrientViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = "recipe"
 
